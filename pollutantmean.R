d947657c58bc44f602ec15b07ff070f4c18bcff7
andrea-biaggi <andrea.biaggi.mi@gmail.com>
2015-10-20 14:11:33 +0200

    versione definitiva
    Aggregazione dei soli file necessari specificati da 'id', con aumento della velocità ed ottimizzazione delle risorse.

--- pollutantmean.R
+++ pollutantmean.R
@@ -2,12 +2,10 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
     files_full <- list.files(directory, full.names = TRUE)
     summary(files_full)
     files_list <- vector(mode = "list", length = length(files_full))
-    for (i in seq_along(files_full)) {
+    for (i in id) {
         files_list[[i]] <- read.csv(files_full[i])
     }
     output <- do.call(rbind,files_list)
-    for (x in id) {
-        data_subset <- subset(output, ID == x)
-    }    
+    data_subset <- subset(output, ID %in% id)
     mean(data_subset[, pollutant], na.rm=TRUE)
 }
